d0ccc92324d686fced1002a131edd76db483162d
Roman Medvedev <roman.medvedev77@gmail.com>
2022-05-16 19:39:23 -0400

    fix style according to pretier

--- src/utilities/requestController.js
+++ src/utilities/requestController.js
@@ -1,16 +1,14 @@
 export default async function getData(prompt) {
   try {
-    const response = await fetch(
-      "https://api.openai.com/v1/engines/text-curie-001/completions",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
-        },
-        body: JSON.stringify(prompt),
-      }
-    )
+    const response = await fetch('https://api.openai.com/v1/engines/text-curie-001/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        // eslint-disable-next-line no-undef
+        Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`
+      },
+      body: JSON.stringify(prompt)
+    })
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
